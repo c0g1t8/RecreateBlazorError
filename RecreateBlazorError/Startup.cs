00e8a26b20db7b5a0f9ecc7afc8348ee6f8adc6a
Gary Chan <gary.chan@c0g1t8.com>
2020-02-22 12:21:25 -0800

    Make minimum work first

--- RecreateBlazorError/Startup.cs
+++ RecreateBlazorError/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HIH.Core.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -29,6 +30,7 @@ namespace RecreateBlazorError
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<IPersonService, PersonService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
